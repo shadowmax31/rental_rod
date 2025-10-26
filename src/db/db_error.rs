6f593e0d021151520a90da5186f4c4a3db107d59
Etienne Beaulieu <etienne.beaulieu@praxar.com>
2025-10-26 16:02:59 -0400

    Add thiserror

--- src/db/db_error.rs
+++ src/db/db_error.rs
@@ -1,33 +1,15 @@
 //! This modules contains everything related to error handling
 
-use std::fmt;
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 /**
  * This is the global type used for handling errors
  */
 pub enum DbError {
-    /**
-     * Basic String error
-     */
+    #[error("{0}")]
     Custom(String),
-    /**
-     * Reference to a std::io::Error
-     */
-    IoError(std::io::Error),
-}
 
-impl From<std::io::Error> for DbError {
-    fn from(error: std::io::Error) -> Self {
-        DbError::IoError(error)
-    }
-}
-
-impl fmt::Display for DbError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            DbError::Custom(s) => write!(f, "{}", s),
-            DbError::IoError(e) => write!(f, "{:?}", e),
-        }
-    }
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
 }
